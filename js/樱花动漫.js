5da07ada5d468abd9c4f632b8383522d9c8f46be
wudong6809 <123217301+wudong6809@users.noreply.github.com>
2023-12-30 21:36:35 +0800

    Update 樱花动漫.js

--- js/樱花动漫.js
+++ js/樱花动漫.js
@@ -1,8 +1,8 @@
 // https://m.agemys.org/#/
 
 var rule = {
-	title:'AGE动漫',
-	host:'https://api.agemys.org',
+	title:'樱花动漫',
+	host:'https://www.iyhdm.com/',
 	homeUrl:'/v2/update?page=1&size=30',
 	url:'/v2/catalog?genre=fyclass&fyfilter&page=fypage&size=10',
 	searchUrl:'/v2/search?query=**&page=fypage',
